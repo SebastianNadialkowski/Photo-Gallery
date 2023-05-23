c4f45178534dcfb51baf30113d872aaea83617c5
Sebastian Nadiałkowski <Sirskuty@gmail.com>
2023-05-23 22:18:53 +0200

    behavior update

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ const body = document.querySelector('body');
 const imagesPaths = [
   'img/kukurydza.jpg',
   'img/lato.jpg',
-  'img/zima.jpg'
+  'img/zima.jpg',
+  'img/zielen.jpg',
+  'img/klos.jpg',
+  'img/zachod.jpg',
   // Add more image paths if desired
 ];
 
@@ -25,44 +28,29 @@ images.forEach(image => {
   image.addEventListener('mouseenter', () => {
     image.classList.add('hovered');
     body.classList.add('hovered');
+    
+    // Check if device is mobile
+    if (window.innerWidth < 768) {
+      // Scale up the image within the screen width
+      const screenWidth = window.innerWidth;
+      const imageWidth = image.offsetWidth;
+      const scaleFactor = screenWidth / imageWidth;
+      
+      image.style.transform = `scale(${scaleFactor})`;
+    } else {
+      // Scale up the image
+      image.style.transform = 'scale(1.5)';
+    }
+    
+    image.style.transition = 'transform 0.3s ease';
   });
 
   image.addEventListener('mouseleave', () => {
     image.classList.remove('hovered');
     body.classList.remove('hovered');
-  });
 
-  image.addEventListener('click', () => {
-    // Remove existing enlarged images
-    const enlargedImages = document.querySelectorAll('.enlarged');
-    enlargedImages.forEach(enlargedImage => {
-      gallery.removeChild(enlargedImage);
-    });
-
-    // Create enlarged image
-    const enlargedImage = document.createElement('img');
-    enlargedImage.src = image.src;
-    enlargedImage.classList.add('enlarged');
-    gallery.appendChild(enlargedImage);
-
-    enlargedImage.addEventListener('load', () => {
-      // Center the enlarged image in the gallery
-      const galleryRect = gallery.getBoundingClientRect();
-      const enlargedImageRect = enlargedImage.getBoundingClientRect();
-      const offsetX = galleryRect.left + galleryRect.width / 2 - enlargedImageRect.left - enlargedImageRect.width / 2;
-      const offsetY = galleryRect.top + galleryRect.height / 2 - enlargedImageRect.top - enlargedImageRect.height / 2;
-
-      enlargedImage.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-    });
-
-    gallery.classList.add('enlarged');
-
-    enlargedImage.addEventListener('click', event => {
-      // Remove the enlarged image when clicked
-      if (event.target.classList.contains('enlarged')) {
-        gallery.removeChild(event.target);
-        gallery.classList.remove('enlarged');
-      }
-    });
+    // Reset the image scale
+    image.style.transform = 'scale(1)';
   });
 });
+
